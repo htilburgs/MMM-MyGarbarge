f789513f07e75e6cb58a69085ecd4a1d9e625a9a
Harm Tilburgs <harm@tilburgs.com>
2025-01-06 19:50:03 +0100

    Create MMM-MyGarbage.js

---
+++ MMM-MyGarbage.js
@@ -0,0 +1,50 @@
+Module.register("MMM-MyGarbage", {
+  // Default module config.
+  defaults: {
+    updateInterval: 3600000, // 1 hour in milliseconds
+  },
+
+  start: function () {
+    this.getPickupInfo();
+    setInterval(() => this.getPickupInfo(), this.config.updateInterval);
+  },
+
+  getPickupInfo: function () {
+    this.sendSocketNotification("GET_PICKUP_INFO");
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "PICKUP_INFO") {
+      this.updatePickupInfo(payload);
+    }
+  },
+
+  updatePickupInfo: function (pickupData) {
+    this.loaded = true;
+    this.pickupData = pickupData;
+    this.updateDom();
+  },
+
+  getDom: function () {
+    const wrapper = document.createElement("div");
+
+    if (!this.loaded) {
+      wrapper.innerHTML = "Loading...";
+      return wrapper;
+    }
+
+    if (this.pickupData && this.pickupData.length > 0) {
+      const list = document.createElement("ul");
+      this.pickupData.forEach((entry) => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `Date: ${entry.date}, Type: ${entry.type}`;
+        list.appendChild(listItem);
+      });
+      wrapper.appendChild(list);
+    } else {
+      wrapper.innerHTML = "No garbage pickup information available.";
+    }
+
+    return wrapper;
+  }
+});
